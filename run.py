dee9f7f7e1d56a5792f872a82ac7c2dbfb179ede
Olle Ljung <olle.ljung@gmail.com>
2024-04-22 00:43:14 +0200

    Removes unused code

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ from pathlib import Path
 import requests
 from dotenv import load_dotenv
 import sys
+from time import sleep
 
 load_dotenv()
+
+backup_directory = "/var/lib/pterodactyl/backups/"
 
 pterodactyl_url = os.getenv('PTERODACTYL_URL')
 pterodactyl_token = os.getenv('PTERODACTYL_AUTH_TOKEN')
@@ -18,12 +21,10 @@ ptero_headers = {
 # Backups are stored in ptero: 01f3e8d9-1d9c-427d-b7ed-f128f3f173a5.tar.gz
 # Intented to be used on one of the Node(s).
 def get_all_filesystem_backups():
-    filesystem_backups_dir = "/var/lib/pterodactyl/backups/"
-    files_in_backup = os.listdir(filesystem_backups_dir)
+    files_in_backup = os.listdir(backup_directory)
     backup_files = [
         Path(Path(f).stem).stem for f in files_in_backup if f.endswith('.tar.gz')]
     return backup_files
-
 
 def backups_not_in_ptero():
     ptero_backups = get_all_backups_from_ptero()
@@ -34,23 +35,6 @@ def backups_not_in_ptero():
         if not each_local_backup in ptero_backups.keys():
             backups_not_in_ptero_list.append(each_local_backup)
     return backups_not_in_ptero_list
-
-
-###
-# WARNING
-# - This will always report a list of backups if there are multiple nodes configured in Pterodactyl
-# Due to backups from servers on other nodes not being in the filesystem of this node.
-###
-def backups_not_in_filesystem():
-    ptero_backups = get_all_backups_from_ptero()
-    local_backups = get_all_filesystem_backups()
-    # Getting all the uuids of the ptero backups not on filesystem
-    backups_not_in_filesystem_list = []
-    for each_ptero_backup in ptero_backups.keys():
-        if not each_ptero_backup in local_backups:
-            backups_not_in_filesystem_list.append(each_ptero_backup)
-    return backups_not_in_filesystem_list
-
 
 def get_all_backups_from_ptero():
     all_backups_from_ptero = {}
@@ -67,8 +51,15 @@ def get_all_backups_from_ptero():
         }
     return all_backups_from_ptero
 
+def delete_file_from_filesystem(path):
+    os.remove(path)
+
 if __name__ == "__main__":
     # not_filesystem_backups = backups_not_in_filesystem()
     # print(not_filesystem_backups)
     not_ptero_backups = backups_not_in_ptero()
-    print(not_ptero_backups)
+    for backup_uuid in not_ptero_backups:
+        backup_path = f'{backup_directory}{backup_uuid}.tar.gz'
+        print(f"Deleting nonexistent backup '{backup_path}' from filesystem.")
+        #delete_file_from_filesystem(backup_path)
+        sleep(1)
